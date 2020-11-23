25733dc55b0a165ae042c1d38b1003e8aa50ae22
Yoganshu-Ojha <71267025+Yoganshu-Ojha@users.noreply.github.com>
2020-11-23 13:50:57 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  readFinishedCars(){
+    var finishedCars = database.ref('finishedCars');
+    finishedCars.on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  static updateFinishedCars(rank){
+    database.ref('/').update({
+      finishedCars:rank
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance,
+      rank:this.rank
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
